428a6efd838df9d55afaf1c3a8c58c05714a46f4
Pierre Vinet <pierre.vinet17@gmail.com>
2025-07-28 17:32:29 +0200

    feat: server page next top loader

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import { AppSidebar } from "@/components/general/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
 import { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -16,6 +17,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <AppSidebar />
         <SidebarInset className="bg-sidebar flex-1 min-w-0">
           <Toaster richColors theme="light" />
+          <NextTopLoader
+            color="var(--primary)"
+            initialPosition={0.1}
+            crawlSpeed={200}
+            height={5}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+          />
           {children}
         </SidebarInset>
       </div>
