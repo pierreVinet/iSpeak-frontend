e6d304478934411f463563da69c8fde6c5ffc70b
Pierre Vinet <pierre.vinet17@gmail.com>
2025-07-30 12:33:35 +0200

    fix: update PostHog UI host URL using env variable

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,8 +2,8 @@ import posthog from "posthog-js";
 
 // Automatically initialized by Next.js at runtime
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://eu.i.posthog.com",
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+  ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
   defaults: "2025-05-24",
   capture_exceptions: true, // Enables error tracking
   person_profiles: "always",
