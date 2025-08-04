16ee9423a7c411c49d9726de827b42d95af360c2
Pierre Vinet <pierre.vinet17@gmail.com>
2025-08-04 13:36:44 +0200

    feat: intrument-client production only

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,11 +1,12 @@
 import posthog from "posthog-js";
 
 // Automatically initialized by Next.js at runtime
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
-  ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
-  defaults: "2025-05-24",
-  capture_exceptions: true, // Enables error tracking
-  person_profiles: "always",
-  debug: process.env.NODE_ENV === "development",
-});
+if (process.env.NODE_ENV === "production") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+    defaults: "2025-05-24",
+    capture_exceptions: true, // Enables error tracking
+    person_profiles: "always",
+  });
+}
